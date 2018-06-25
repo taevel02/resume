636ca721b60bbdb2f426091978cc0ae72e130481
Taehoon <kth020315@naver.com>
2018-06-26 08:09:19 +0900

    Edit Bio

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .content
     h2 Bio
-    .level-left 갑자기 꼴리는거 해야 적성이 풀리는 열일곱 개발자<br />수학보다는 우분투가 좋아요><
+    .level-left 갑자기 꼴리는거 해야 적성이 풀리는 열일곱<br />수학보다는 우분투가 좋아요><
 </template>
 
 <style lang="scss" scoped>
