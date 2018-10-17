c7174ba4d767fb9e4c0a8bcecd59e368ecb980cb
taehoon02 <taevel02@naver.com>
2018-10-17 20:44:10 +0900

    circleci

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,5 +1,4 @@
-export default [
-  {
+export default [{
     year: 2018,
     projects: [{
       name: 'weaver',
@@ -7,31 +6,31 @@ export default [
       period: '2018.07 -',
       description: '바다 생태계를 보존하기 위한 산호 환경 조성 로봇 프로젝트',
       tags: ['vue.js', 'node.js', 'firebase', 'arduino', 'robot']
-    },{
+    }, {
       name: 'dimibob',
       href: 'https://github.com/taehoon02/dimibob',
       period: '2018.06',
       description: '학교 급식을 표시해주는 안드로이드 위젯',
       tags: ['kotlin', 'android', 'okhttp']
-    },{
+    }, {
       name: 'kakao-chatbot_dimibob',
       href: 'https://github.com/taehoon02/kakao-chatbot_dimibob',
       period: '2018.06',
       description: '학교 급식을 알려주는 카카오톡 챗봇',
       tags: ['php', 'node.js']
-    },{
+    }, {
       name: 'clober',
       href: 'https://github.com/taehoon02/clober',
       period: '2018.05',
       description: '스타트업 위크엔드 출품작',
       tags: ['java', 'android', 'firebase']
-    },{
+    }, {
       name: 'manito_game',
       href: 'https://github.com/taehoon02/manito_game',
       period: '2018.04',
       description: '수기로 뽑는 마니또를 온라인에서 뽑을 수 있도록 개선한 서비스',
       tags: ['semantic', 'javascript']
-    },{
+    }, {
       name: 'ModPE_WorldEdit',
       href: 'https://github.com/taehoon02/ModPE_WorldEdit',
       period: '2018.01',
@@ -48,7 +47,7 @@ export default [
       period: '2017.10',
       description: 'Arduino 8x8x8 LED CUBE',
       tags: ['arduino']
-    },{
+    }, {
       name: 'ModPE_PandongEditor',
       href: 'https://github.com/taehoon02/ModPE_PandongEditor',
       period: '2017.02',
