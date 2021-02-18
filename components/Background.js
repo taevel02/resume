291e87ef3205944b7adaed0247a12f9c31a8f235
taevel02 <taevel02@gmail.com>
2021-02-19 01:33:04 +0900

    feat: draw backgroud svg rectangle

---
+++ components/Background.js
@@ -0,0 +1,35 @@
+import styles from "../styles/Background.module.css";
+
+export default function Background() {
+  return (
+    <div className={styles.container}>
+      <svg
+        width="100%"
+        height="100vh"
+        viewBox="0 0 1920"
+        fill="none"
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="3%"
+          y="145%"
+          width="580"
+          height="580"
+          rx="65"
+          transform="rotate(-45)"
+          fill="#EEEEF0"
+        />
+        <rect
+          x="-5%"
+          y="20%"
+          width="1130"
+          height="815"
+          rx="65"
+          transform="rotate(-45 -255 223)"
+          fill="#FFC2BD"
+        />
+      </svg>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Home.module.css";
 
+import Background from "../components/Background";
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div />
+    <div>
+      <Background />
+      <div className={styles.container}>
+        <p>
+          Magna dolore cillum enim ut sunt adipisicing ex consectetur ullamco.
+        </p>
+      </div>
     </div>
   );
 }
