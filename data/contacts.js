431f15f26557482ad5a2aeb756a70f6ae3de53a8
taevel02 <taevel02@gmail.com>
2021-02-19 21:13:26 +0900

    feat: update contact data

--- data/contacts.js
+++ data/contacts.js
@@ -1,19 +1,11 @@
 export default [
   {
     text: "Republic of Korea",
-    href: "#",
+    href: "/",
   },
   {
     text: "taevel02@gmail.com",
     href: "mailto:taevel02@gmail.com",
-  },
-  {
-    text: "github",
-    href: "https://github.com/taevel02",
-  },
-  {
-    text: "brunch",
-    href: "https://brunch.co.kr/@taevel02",
   },
   {
     text: "instagram",
