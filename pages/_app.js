b32ebd08a388226897fbef5fccf07b95feefaf30
taevel02 <taevel02@gmail.com>
2021-04-11 00:04:28 +0900

    delete: other unnecessary files

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import "@fortawesome/fontawesome-free/css/fontawesome.css";
-import "@fortawesome/fontawesome-free/css/solid.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
