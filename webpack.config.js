eb0747ce61887842f0d4eb921d95e26de2f6fa85
taevel02 <taevel02@gmail.com>
2021-04-11 00:02:58 +0900

    feat: setting webpack 5 - esbuild loader

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+
+  output: {
+    filename: "main.js",
+    path: `${__dirname}/dist`,
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    port: 3000,
+    contentBase: "./",
+    publicPath: "/dist",
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "tsx",
+          target: "es2015",
+        },
+      },
+    ],
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
