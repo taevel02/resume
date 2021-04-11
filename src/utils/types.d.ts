06a169ab520ab401ee66faf9a0d9c4116d6ff73a
taevel02 <taevel02@gmail.com>
2021-04-12 01:22:23 +0900

    feat: create resume data; json

---
+++ src/utils/types.d.ts
@@ -0,0 +1,26 @@
+export interface IInfo {
+  title: string;
+  subtitle: string;
+  description: string;
+}
+
+export interface IContact {
+  text: string;
+  href: string;
+}
+
+export interface IExperience {
+  period: string;
+  location?: string;
+  name: string;
+  role: string;
+  description?: string;
+  href?: string;
+}
+
+export interface IAward {
+  date: string;
+  content: string;
+  project?: string;
+  host?: string;
+}
