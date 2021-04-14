27edce6aa601303e3088a0929532c1e6b116032b
taevel02 <taevel02@gmail.com>
2021-04-15 02:13:31 +0900

    feat: delete ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
+	mode: 'development',
 
-  entry: "./src/index.tsx",
+	entry: './src/index.tsx',
 
-  output: {
-    filename: "main.js",
-    path: `${__dirname}/dist`,
-  },
+	output: {
+		filename: 'main.js',
+		path: `${__dirname}/dist`,
+	},
 
-  devtool: "source-map",
+	devtool: 'source-map',
 
-  devServer: {
-    historyApiFallback: true,
-    inline: true,
-    hot: true,
-    port: 3000,
-    contentBase: "./",
-    publicPath: "/dist",
-  },
+	devServer: {
+		historyApiFallback: true,
+		inline: true,
+		hot: true,
+		port: 3000,
+		contentBase: './',
+		publicPath: '/dist',
+	},
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        loader: "esbuild-loader",
-        options: {
-          loader: "tsx",
-          target: "es2015",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'esbuild-loader',
+				options: {
+					loader: 'tsx',
+					target: 'es2015',
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
 
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+	plugins: [new webpack.HotModuleReplacementPlugin()],
 };
