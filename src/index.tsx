114c37a6278a45180a975b80a088fb2df57d2a07
taevel02 <taevel02@gmail.com>
2021-04-16 01:50:23 +0900

    feat: apply global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import App from './components/App';
+import App from './pages/App';
+
+import './scss/reset.scss';
+import './scss/global.scss';
 
 ReactDom.render(<App />, document.getElementById('app'));
