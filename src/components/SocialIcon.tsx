d8d27e45b193ab74efa3d3d328eee6f8c8740b5f
taevel02 <taevel02@gmail.com>
2021-04-21 02:13:55 +0900

    feat: open new window on focus when click link

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
+
+import openNewWindow from '../utils/openNewWindow';
 import { ISocial } from '../utils/types';
 
 const Section = styled('div')`
@@ -38,7 +40,7 @@ const SocialIcon: React.FC<SocialIconProps> = ({ socials }) => {
 	return (
 		<Section>
 			{socials.map(({ href, text }) => (
-				<CircleBox href={href}>
+				<CircleBox onClick={() => openNewWindow(href)}>
 					<Icon src={`../../public/static/${text}.png`} alt={text} />
 				</CircleBox>
 			))}
