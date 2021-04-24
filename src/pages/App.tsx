4532df0d81bd6cf7f31dc58e0f35f5163fe35527
taevel02 <taevel02@gmail.com>
2021-04-25 01:52:24 +0900

    feat: combine components

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,46 @@
 import * as React from 'react';
-import SocialIcon from '../components/SocialIcon';
+import styled from '@emotion/styled';
 
-import { ISocial } from '../utils/types';
+import Header from '../components/Header';
+import Experience from '../components/Experience';
+
+import { ISocial, IExperience } from '../utils/types';
 
 const socials: ISocial[] = require('../data/social.json');
+const experiences: IExperience[] = require('../data/experience.json');
+
+const Content = styled('div')`
+	display: flex;
+	flex-direction: column;
+	max-width: 800px;
+	margin: 0 auto;
+
+	> :not(:last-child) {
+		margin-bottom: 50px;
+	}
+`;
+
+const Footer = styled('footer')`
+	padding: 1em 0;
+`;
+
+const FooterText = styled('span')`
+	font-size: 11px;
+	font-weight: 100;
+	text-rendering: optimizeLegibility;
+	color: #aaa;
+	line-height: 13px;
+`;
 
 const App: React.FC = () => {
 	return (
-		<div>
-			<SocialIcon socials={socials} />
-		</div>
+		<Content>
+			<Header socials={socials} />
+			<Experience experiences={experiences} />
+			<Footer>
+				<FooterText>BY. Taehoon</FooterText>
+			</Footer>
+		</Content>
 	);
 };
 
