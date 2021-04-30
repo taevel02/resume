d5c02a3d7de633cf23b32f2631a6ee4b05f089b5
taevel02 <taevel02@gmail.com>
2021-05-01 01:06:45 +0900

    feat: change how to apply font-awesome

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import App from './pages/App';
 
+import '@fortawesome/fontawesome-free/js/all.min.js'
 import './scss/reset.scss';
 import './scss/global.scss';
 
